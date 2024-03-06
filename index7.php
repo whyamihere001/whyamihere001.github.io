f0f4d26ec35e924d1a5c96beeb133259e6044ba3
whyamihere001 <156673284+whyamihere001@users.noreply.github.com>
2024-03-06 21:31:37 +0800

    Add files via upload

---
+++ index7.php
@@ -0,0 +1,11 @@
+<nav>
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="index2.html">About</a></li>
+    <li><a href="index3.html">Services</a></li>
+    <li><a href="index4.html">page 4</a></li>
+    <li><a href="index6.html">page 5</a></li>
+    
+   
+  </ul>
+</nav>
